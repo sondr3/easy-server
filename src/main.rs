7c11daae46122517f86a23e4dec7348a60b45a08
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-09-13 00:35:02 +0200

    Some quick and dirty file system polling
    Infinite loops

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
 extern crate colored;
 extern crate rouille;
+extern crate notify;
 
 use colored::*;
+use rouille::Response;
+use notify::{Watcher, RecursiveMode, watcher};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
+fn files() {
+    let (tx, rx) = channel();
+
+    let mut watcher = watcher(tx, Duration::from_secs(1)).unwrap();
+
+    watcher.watch(".", RecursiveMode::Recursive).unwrap();
+
+    loop {
+        match rx.recv() {
+            Ok(event) => println!("{:?}", event),
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+}
 
 fn main() {
     println!("Listening on {}", "localhost:8118".to_string().blue());
@@ -10,11 +30,13 @@ fn main() {
         {
             let response = rouille::match_assets(&request, ".");
 
+            files();
+
             if response.is_success() {
                 return response;
             }
         }
 
-        rouille::Response::html("404!").with_status_code(404)
+        Response::html("404!").with_status_code(404)
     });
 }
