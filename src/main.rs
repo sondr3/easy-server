f7ce0a060956743edc41a215b402a2ace0c06243
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-09-13 00:27:14 +0200

    This is something I can work with

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate colored;
+extern crate rouille;
+
+use colored::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Listening on {}", "localhost:8118".to_string().blue());
+
+    rouille::start_server("localhost:8118", move |request| {
+        {
+            let response = rouille::match_assets(&request, ".");
+
+            if response.is_success() {
+                return response;
+            }
+        }
+
+        rouille::Response::html("404!").with_status_code(404)
+    });
 }
